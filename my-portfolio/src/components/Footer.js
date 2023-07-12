e60f46dbef59ea2b527abe666fe7a94439e8da5a
Jovan <jokouakou@gmail.com>
2023-07-12 18:09:04 -0400

    Update Footer.js

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 
 function Footer() {
     return (
-        <>
-        </>
+        <div className="py-4 text-center bg-primary text-indigo-500">
+             &copy; 2023 jovan kouakou all rights reserved
+        </div>
     );
 };
 
