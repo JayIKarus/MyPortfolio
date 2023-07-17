b129f2c72669fd88b0a8bfbf71caca9b738f7714
Jovan <jokouakou@gmail.com>
2023-07-17 14:45:16 -0400

    Added Framer Motion
    Added Framer Motion package.
    
    Added animations to Navigation bar.
    
    Added animations to Hero social links.
    
    Adjusted the look of projects in Projects.

--- my-portfolio/src/components/Hero.js
+++ my-portfolio/src/components/Hero.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { animate, motion } from "framer-motion";
 import JovanGlint from "../assets/JovanGlint_NoBack.png";
 
 import {
@@ -13,49 +14,59 @@ function Hero() {
             <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
                 <div className="hero-info pb-5 md:pb-0">
                     <h1 className="text-4xl lg:text-8xl">
-                        Hi, <br /> I'm <span className="text-accent text-9xl">Jovan</span>, a<br />
+                        Hi, <br /> I'm <motion.span className="text-accent text-9xl inline-block" whileHover={{ scale: 1.1}} initial={{ scale: 1}}>Jovan</motion.span>, <br />
                         Developer and Designer
                     </h1>
 
                     <div className="flex py-8">
-                        <a
+                        <motion.a whileHover={{y: -3}}
                             href="https://www.instagram.com/bon_kojo/"
                             className="pr-4 inline-block text-accent hover:text-stone-200"
                         >
                             {" "}
                             <AiOutlineInstagram size={40} /> {" "}
-                        </a>
-                        <a
+                        </motion.a>
+                        <motion.a whileHover={{y: -3}}
                             href="https://github.com/JayIKarus"
                             className="pr-4 inline-block text-accent hover:text-stone-200"
                         >
                             {" "}
                             <AiOutlineGithub size={40} /> {" "}
-                        </a>
-                        <a
+                        </motion.a>
+                        <motion.a whileHover={{y: -3}}
                             href="https://www.linkedin.com/in/jovan-kouakou/"
                             className="pr-4 inline-block text-accent hover:text-stone-200"
                         >
                             {" "}
                             <AiFillLinkedin size={40} /> {" "}
-                        </a>
+                        </motion.a>
                     </div>
                     
-                    <a
+                    <a 
                         href="/#projects"
-                        className=" btn bg-accent border-2 border-[#74BB77] text-stone-200 px-6 py-3 drop-shadow-xl hover:bg-transparent"
+                        className="btn bg-accent border-2 border-[#74BB77] text-stone-200 px-6 py-3 drop-shadow-xl hover:bg-transparent"
                     >
                         See Projects
                     </a>
                 </div>
 
-                <div className="hero-img">
+                <motion.div 
+                    className="hero-img" 
+                    animate={{
+                        x: [0,Math.random(0,20), 15, 15, 0, -15, -15, 0], 
+                        y: [0, 15, -15, 0, 15, -15, 0]}}
+                    transition={{
+                        ease: "easeInOut",
+                        duration: 15,
+                        repeat: Infinity,
+                        }}
+                    >
                     <img 
                         src={JovanGlint}
                         alt="got my eyes on you"
                         className="lgw-[80%] ml-auto h-116 w-116 rounded-full drop-shadow-2xl hover:shadow-inner"
                     />
-                </div>
+                </motion.div>
             </div>
         </section>
     );
--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -10,7 +10,7 @@ function Projects() {
             img: selfReflection,
             title: "Self-Refelection",
             desc: " A Unity 3D Game Project made by Flaming Bee Studios. I served as the Art Director, UI/UX Developer and a Programmer.",
-            live: "https://sites.google.com/view/flaming-bee-studios/home?authuser=0", 
+            live: "https://sites.google.com/view/flaming-bee-studios/home", 
         },
         {
             img: firmwareInfirmary,
@@ -27,7 +27,7 @@ function Projects() {
                     <h2 className="tracking-tight text-6xl font-bold mb-5 border-b-[5px] w-[300px] border-stone-800 border-opacity-50 pb-1 drop-shadow-lg">
                         Projects
                     </h2>
-                    <p className="pb-5">
+                    <p className="pb-5 w-3/6">
                         Most of my recent projects have been games built in Unity, models, textures, and animations built in Maya and Substance Painter, and scenes created in Unreal. I'm always trying to something and create something cool.
                     </p>
                 </div>
@@ -38,9 +38,9 @@ function Projects() {
                 {projects.map((project, i) => {
                     return (
                         <a href={project.live} target="_blank">
-                            <div className="relative" key={i}>
-                                <image src={project.img} alt={project.title} />
-                                <div className="flex absolute left-0 right-0 top-[20px] bottom-0 mx-auto w-[90%]  h-32 bg-primary opacity-0 duration-500 justify-center flex-col hover:opacity-100">
+                            <div className="relative group" key={i}>
+                                <img src={project.img} alt={project.title} className="opacity-100 duration-500 group-hover:opacity-25"/>
+                                <div className="flex absolute left-0 right-0 top-1/3 bottom-1/3 mx-auto w-[90%] h-32 opacity-0 duration-500 justify-center flex-col group-hover:opacity-100">
                                     <p className="py-5 text-center font-bold px-2 text-white">
                                         {project.desc}
                                     </p>
